67d235dff2c7ad4333227c1c8d6e06bc1183eedf
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2018-02-22 17:21:54 +0100

    Added parse_complex and pixel_to_point

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
+/// Given a string and a separator it parses the string and divides the string by the separator.
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
@@ -35,6 +36,29 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
 }
 
 
+/// Given a string it creates a complex numbers using parse_pair()
+fn parse_complex(s:&str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re, 
+                           upper_left.im - lower_right.im);
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
 // TESTS
 
 #[test]
@@ -47,3 +71,19 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25, 0.0625"), 
+               Some(Complex { re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), 
+                              (25, 75),
+                              Complex { re: -1.0, im: 1.0 },
+                              Complex { re: 1.0, im: -1.0}),
+               Complex { re: -0.5, im: -0.5 });
+}                         
